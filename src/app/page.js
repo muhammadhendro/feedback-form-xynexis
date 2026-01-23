09779bc42693d51098ef3c060cc0a07cc068c726
flintz <0xflintz@gmail.com>
2026-01-23 19:47:46 +0700

    update

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import FeedbackForm from '../components/FeedbackForm';
 
 export default function Home() {
   return (
-    <main className="min-h-screen py-12 px-4 sm:px-6 lg:px-8">
+    <main className="min-h-screen">
       <FeedbackForm />
     </main>
   );
